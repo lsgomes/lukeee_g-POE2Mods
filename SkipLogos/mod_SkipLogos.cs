94fb6cc0da244b604b013b1b6a531c338282f7b5
lsgomes <lucas.gomes.canoas@gmail.com>
2024-12-23 21:22:25 -0300

    Skip Logos Mod

---
+++ SkipLogos/mod_SkipLogos.cs
@@ -0,0 +1,26 @@
+﻿using Game;
+using Patchwork;
+using Onyx;
+using UnityEngine;
+using Game.GameData;
+using Game.UI;
+
+namespace lukeee_g_DisableScreenShake
+{
+    [ModifiesType]
+    public class Lukeee_gCompanyIntroductionManager : Game.UI.CompanyIntroductionManager
+    {
+
+        [ModifiesMember("Start")]
+        public void Start()
+        {
+            // Set m_skipIntro to true to skip logos
+            this.m_skipIntro = true;
+            Time.timeScale = 1f;
+            this.ObsidianLogoTexture.alpha = 0f;
+            this.PublisherTexture.alpha = 0f;
+            GameUtilities.CreateGlobalPrefabObject();
+            base.StartCoroutine(this.IntroCoroutine());
+        }
+    }
+}
